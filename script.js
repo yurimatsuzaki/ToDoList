c37f41fae9e3865138c60cde7dc99c4810851b50
yurimatsuzaki <155598762+yurimatsuzaki@users.noreply.github.com>
2024-09-23 19:58:12 -0300

    ajustando a logica de delete pt1

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 const lista = document.getElementById('tarefas')
+/*
+let TarefaDiv = document.querySelectorAll('div.divTarefa')
+let chacar = document.querySelectorAll('input.check')
+*/
 const del = document.getElementsByTagName('button')[0]
 const popUp1 = document.getElementsByTagName('dialog')[0]
 const popUp2 = document.getElementsByTagName('dialog')[1]
 const popUp3 = document.getElementsByTagName('dialog')[2]
-
-let verificador = 0
-
-function apagar(){
-    verificador = 1
-    console.log('ooo')
-}
-function naoApagar(){
-    verificador = 2
-}
+const BtOK1 = document.getElementsByClassName('ok')[0]
+const BtOK2 = document.getElementsByClassName('ok')[1]
+const BtSIM = document.getElementById('sim')
+const BtNAO = document.getElementById('nao')
 
 function add() {
-    verificador = 3
-
     const divTarefa = document.createElement('div')
     divTarefa.classList.add('divTarefa')
     
@@ -24,9 +20,8 @@ function add() {
     textArea.classList.add('tarefa')
     textArea.placeholder = 'Digite sua tarefa...'
     textArea.maxLength = 100
-    textArea.style.height = 'auto'
     
-    const check = document.createElement('input')
+    const check  = document.createElement('input')
     check.setAttribute('type', 'checkbox')
     check.classList.add('check')
     
@@ -35,26 +30,40 @@ function add() {
     
     lista.appendChild(divTarefa)
     textArea.focus()
-    
+
     check.addEventListener('click', function(){
         if (check.checked) {
             textArea.style.textDecoration = 'line-through'
-            textArea.style.color = 'rgb(77, 77, 77)'
-            textArea.style.backgroundColor = 'rgb(26, 0, 51)'
+            textArea.style.color = 'white'
+            textArea.style.backgroundColor = '#42008093'
         }else{
             textArea.style.textDecoration = 'none'
             textArea.style.color = 'white'
             textArea.style.backgroundColor = 'rgb(66, 0, 128)'
         }
     })
-
-        if(check.checked){
-            if(verificador == 1){
-                divTarefa.remove()
-                popUp3.close()
-            }
-            else if(verificador == 2){
-                 popUp3.close()
-            }
-        }
 }
+
+
+del.addEventListener('click', function(){
+    if(check.checked == false){
+        popUp1.showModal()
+
+        BtOK1.onclick = function(){
+            popUp1.close()
+        }
+    }   
+    if(check.checked == true){
+        popUp3.showModal()
+        
+        BtSIM.onclick = function(){
+            divTarefa.remove()
+            popUp3.close()
+        }
+        
+        BtNAO.onclick = function(){                 
+            popUp3.close()
+        }
+        chacar.checked = false
+    }
+})
