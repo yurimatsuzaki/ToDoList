5fee02d990ac551873b8a1b9eb3004f6a082f418
yurimatsuzaki <155598762+yurimatsuzaki@users.noreply.github.com>
2024-10-04 18:08:52 -0300

    reiniciando o projeto

--- script.js
+++ script.js
@@ -1,18 +1,160 @@
-const lista = document.getElementById('tarefas')
-const adicionar = document.getElementById('adicionar')
-const del = document.getElementsByTagName('button')[0]
+const listaTarefas = document.getElementById('listaTarefas')
+const areaBtn = document.getElementById('areaBtn')
+
 const popUp1 = document.getElementsByTagName('dialog')[0]
 const popUp2 = document.getElementsByTagName('dialog')[1]
 const popUp3 = document.getElementsByTagName('dialog')[2]
 const popUp4 = document.getElementsByTagName('dialog')[3]
-const BtOK1 = document.getElementsByClassName('ok')[0]
-const BtOK2 = document.getElementsByClassName('ok')[1]
-const BtOK3 = document.getElementsByClassName('ok')[2]
-const BtSIM = document.getElementById('sim')
-const BtNAO = document.getElementById('nao')
+const BtnOK1 = document.getElementsByClassName('ok')[0]
+const BtnOK2 = document.getElementsByClassName('ok')[1]
+const BtnOK3 = document.getElementsByClassName('ok')[2]
+const BtnSIM = document.getElementById('simDel')
+const BtnNAO = document.getElementById('naoDel')
+
+function criarDivTarefa(){
+    const ckBox = document.createElement('input').setAttribute('type', 'checkbox').classList.add('check')
+    
+    const txtArea = document.createElement('textarea').classList.add('tarefa')
+    txtArea.placeholder = 'Digite sua tarefa...'
+    txtArea.maxLength = 500
+    
+    const divTarefa = document.createElement('div').classList.add('divTarefa')
+
+    divTarefa.appendChild(txtArea)
+    divTarefa.appendChild(ckBox)
+    listaTarefas.appendChild(divTarefa)
+}
+
+function criarBtnSalvar(){
+    const btnSalvar = document.createElement('button').innerText = 'Salvar'
+    btnSalvar.classList.add('salvar')
+
+    return btnSalvar
+}
+
+function salvarTarefa(){
+    const salvarBtn = criarBtnSalvar()
+
+    salvarBtn.onclick = function(){
+        
+    }
+}
+
+
+
+/*
 let controlhe = 0
 let controlhe2 = 0
 let armazem = []
+let armazem2 = []
+let divTask2
+const ck2 = checkbox()
+const AreaTexto1 = AreaTexto()
+const divTask = div()
+const ck = checkbox()
+const salvarTarefa = salve()
+const textArea = AreaTexto()
+
+window.addEventListener('load', function(){
+    for(let i = 0; i < localStorage.length; i++){
+        let chave = localStorage.key(i)
+        armazem2.push(chave)
+
+        let tarefa = String(armazem2[i])
+        
+        divTask.classList.add('divTarefa')
+        
+        textArea.classList.add('tarefa')
+        textArea.value = localStorage.getItem(tarefa)
+        textArea.maxLength = 100
+        
+        ck.setAttribute('type', 'checkbox')
+        ck.classList.add('check')
+        
+        divTask.appendChild(textArea)
+        divTask.appendChild(ck)
+        lista.appendChild(divTask)
+        
+        input()
+        deletar()
+    }
+})
+
+function salvarTask(){
+    salvarTarefa.onclick = function(){
+        if(textArea.value.length == 0){
+            popUp2.showModal()
+            
+            BtOK2.onclick = function(){
+            popUp2.close()
+            }
+        }else{
+            armazem2.push(`task_${String(textArea.value)}`)
+            localStorage.setItem(`task_${String(textArea.value)}`, String(textArea.value))
+            salvarTarefa.remove()
+            controlhe2 = 0
+        }
+    }
+}
+
+function input(){
+    ck.addEventListener('click', function(){
+    if (ck.checked == true) {
+        textArea.style.textDecoration = 'line-through'
+        textArea.style.color = 'white'
+        textArea.style.backgroundColor = '#42008093'
+
+        divTask.classList.add('marcado')
+        ck.classList.add('checado')
+
+        armazem.push(`task_${String(textArea.value)}`)
+        
+        ck2.checked = true
+    }else{
+        textArea.style.textDecoration = 'none'
+        textArea.style.color = 'white'
+        textArea.style.backgroundColor = 'rgb(66, 0, 128)'
+
+        let indice = armazem.indexOf(`task_${String(textArea.value)}`)
+        console.log(indice)
+        armazem.splice(indice)
+        console.log(armazem)
+
+        divTask.classList.remove('marcado')
+        ck.classList.remove('checado')
+    }
+})
+}
+
+    function deletar(){
+    del.addEventListener('click', function(){
+    let divMarcada = document.querySelectorAll('div.divTarefa.marcado')
+    let ckMarcado = document.querySelectorAll('input.check.checado')
+    
+    if(ck.checked == true){
+        popUp3.showModal()
+        BtSIM.onclick = function(){
+            for(let task of armazem){
+                localStorage.removeItem(`${task}`)
+            }
+            divMarcada.forEach(divMarcada => {
+                divMarcada.remove()
+            })
+            ckMarcado.forEach(ckMarcado => {
+                ckMarcado.checked = false
+            })
+            salvarTarefa.remove()
+            controlhe2 = 0
+            popUp3.close()
+            ck2.checked = false
+        }
+        BtNAO.onclick = function(){                 
+            popUp3.close()
+            ck.checked = true
+        }
+    }
+})
+}
 
 function div(){
     const divTarefa = document.createElement('div')
@@ -24,6 +166,11 @@ function checkbox(){
     return check
 }
 
+function AreaTexto(){
+    const textArea = document.createElement('textarea')
+    return textArea
+}
+
 function salve(){
     const salvar = document.createElement('button')
     salvar.innerText = 'Salvar'
@@ -32,107 +179,47 @@ function salve(){
 }
 
 function add() {
-    const divTask = div()
-    const ck = checkbox()
-    const salvarTarefa = salve()
-    const textArea = document.createElement('textarea')
+    const divTask3 = div()
+    const ck3 = checkbox()
+    const salvarTarefa3 = salve()
+    const textArea3 = AreaTexto()
     
-    divTask.classList.add('divTarefa')
+    divTask3.classList.add('divTarefa')
     
-    textArea.classList.add('tarefa')
-    textArea.placeholder = 'Digite sua tarefa...'
-    textArea.maxLength = 100
+    textArea3.classList.add('tarefa')
+    textArea3.placeholder = 'Digite sua tarefa...'
+    textArea3.maxLength = 100
     
-    ck.setAttribute('type', 'checkbox')
-    ck.classList.add('check')
+    ck3.setAttribute('type', 'checkbox')
+    ck3.classList.add('check')
     
     if(controlhe2 == 1){
         popUp4.showModal()
+        
         BtOK3.onclick = function(){
             popUp4.close()
         }
     }else{
-    salvarTarefa.classList.add('salvar')
-    
-    divTask.appendChild(textArea)
-    divTask.appendChild(ck)
-
-    lista.appendChild(divTask)
-    textArea.focus()
-
-    adicionar.appendChild(salvarTarefa)
-    controlhe2 = 1
-    
-    salvarTarefa.onclick = function(){
-        if(textArea.value.length == 0){
-            popUp2.showModal()
-            
-            BtOK2.onclick = function(){
-                popUp2.close()
-            }
-        }
-        else{
-            armazem.push(String(textArea.value))
-            console.log(armazem[controlhe])
-            controlhe++
-            localStorage.setItem(`tarefa${controlhe}`, armazem[controlhe-1])
-            salvarTarefa.remove()
-            controlhe2 = 0
-        }
-    }
-    
-    ck.addEventListener('click', function(){
-        if (ck.checked == true) {
-            textArea.style.textDecoration = 'line-through'
-            textArea.style.color = 'white'
-            textArea.style.backgroundColor = '#42008093'
-
-            divTask.classList.add('marcado')
-            ck.classList.add('checado')
-            
-            ck2.checked = true
-        }else{
-            textArea.style.textDecoration = 'none'
-            textArea.style.color = 'white'
-            textArea.style.backgroundColor = 'rgb(66, 0, 128)'
-
-            divTask.classList.remove('marcado')
-            ck.classList.remove('checado')
-        }
-    })
-
-    del.addEventListener('click', function(){
-        let divMarcada = document.querySelectorAll('div.divTarefa.marcado')
-        let ckMarcado = document.querySelectorAll('input.check.checado')
+        salvarTarefa3.classList.add('salvar')
         
-        if(ck.checked == true){
-            popUp3.showModal()
-            BtSIM.onclick = function(){
-                divMarcada.forEach(divMarcada => {
-                    divMarcada.remove()
-                })
-                ckMarcado.forEach(ckMarcado => {
-                    ckMarcado.checked = false
-                })
-                ck2.checked = false
-                salvarTarefa.remove()
-                controlhe2 = 0
-                popUp3.close()
-
-                localStorage.removeItem(`tarefa${controlhe}`)
-            }
-            BtNAO.onclick = function(){                 
-                popUp3.close()
-                ck.checked = true
-            }
-        }
-    })
-    divTask2 = divTask
+        divTask3.appendChild(textArea3)
+        divTask3.appendChild(ck3)
+        
+        lista.appendChild(divTask3)
+        textArea3.focus()
+        
+        adicionar.appendChild(salvarTarefa3)
+        controlhe2 = 1
+        
+        salvarTask()
+        
+        input()
+        
+        deletar()
+        
+        divTask2 = divTask3
+    }
 }
-}
-
-var divTask2
-const ck2 = checkbox()
 
 del.addEventListener('click', function(){
     if(ck2.checked == false){
@@ -141,4 +228,4 @@ del.addEventListener('click', function(){
             popUp1.close()
         }
     }   
-})
+})*/
